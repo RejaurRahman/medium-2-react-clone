9d0445d0c249505a7249293ed66322dce038d41f
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-11-16 23:59:35 +0000

    UPDATE: logic so post comments are sent to sanity cms

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -3,14 +3,43 @@ import type {
   NextApiRequest,
   NextApiResponse
 } from 'next'
+import sanityClient from '@sanity/client'
 
-export default function createComment(
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  useCDN: process.env.NODE_ENV === 'production',
+  token: process.env.SANITY_API_TOKEN
+}
+
+const client = sanityClient(config)
+
+export default async function createComment(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { _id, name, email, comment } = JSON.parse(req.body)
+
+  try {
+    await client.create({
+      _type: 'comment',
+      name,
+      email,
+      comment,
+      post: {
+        _type: 'reference',
+        _ref: _id
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: `Could not submit comment`, error
+    })
+  }
+
   res.status(200).json(
     {
-      name: 'John Doe'
+      message: 'Comment submitted'
     }
   )
 }
