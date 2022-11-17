9353e6034214ddc5ca848f73021438b6511f7837
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-11-17 00:34:01 +0000

    ADD: comments interface to typings

--- typings.d.ts
+++ typings.d.ts
@@ -4,6 +4,7 @@ export interface Post {
   title: string
   description: string
   body: [object]
+  comments: Comment[]
   author: {
     name: string
     image: string
@@ -15,5 +16,21 @@ export interface Post {
   }
   slug: {
     current: string
+  }
+}
+
+export interface Comment {
+  approved: boolean
+  comment: string
+  email: string
+  name: string
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+  post: {
+    _ref: string
+    _type: string
   }
 }
